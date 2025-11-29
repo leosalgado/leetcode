7c7dbeb8f089d9b2c7f74428445e0b05bf08c5e5
leosalgado <leonardosalgado@protonmail.com>
2025-11-28 21:19:09 -0300

    Build Array from Permutation [cpp]

---
+++ 1920_BuildArrayFromPermutation/cpp/main.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+  vector<int> buildArray(vector<int> &nums) {
+    vector<int> x;
+    for (int i{}; i < nums.size(); i++)
+      x.push_back(nums[nums[i]]);
+    return x;
+  }
+};
